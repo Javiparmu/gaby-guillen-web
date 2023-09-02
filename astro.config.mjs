76eac63dfff230c71966c5ada5e8eca1309a7e97
JaviParMu <mrjavistinson@gmail.com>
2023-09-02 12:11:22 +0200

    fix: middleware

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,9 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react({
+    include: ['**/admin/*'],
+  })],
   output: "hybrid",
   adapter: vercel({
     analytics: true,
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,18 @@
 import { defineMiddleware } from "astro/middleware";
+import { adminPassword, adminUsername } from "./utils/constants";
 
 export const onRequest = defineMiddleware((context, next) => {
     const pathname = context.url.pathname;
 
-    if (pathname.startsWith("/admin")) {
+    if (pathname.startsWith("/34c83614")) {
         const { value: session } = context.cookies.get("session") ?? {};
 
         const { username, password } = JSON.parse(session ?? "{}");
 
-        if (username === "admin" && password === "admin12345" && (pathname === "/admin" || pathname === "/admin/")) {
-            return context.redirect("/admin/dashboard");
-        } else if ((username !== "admin" || password !== "admin12345") && (pathname === "/admin/dashboard" || pathname === "/admin/dashboard/")) {
-            return context.redirect("/admin");
+        if (username === adminUsername && password === adminPassword && (pathname === "/34c83614" || pathname === "/34c83614/")) {
+            return context.redirect("/34c83614/dashboard");
+        } else if ((username !== adminUsername || password !== adminPassword) && (pathname === "/34c83614/dashboard" || pathname === "/34c83614/dashboard/")) {
+            return context.redirect("/34c83614");
         }
     }
 
