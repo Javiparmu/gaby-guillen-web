2d75ff6eff476253eb2e908953cc4ab2dfdf74be
JaviParMu <mrjavistinson@gmail.com>
2023-09-02 19:14:27 +0200

    fix: form method and layout

--- src/components/admin/LoginForm.tsx
+++ src/components/admin/LoginForm.tsx
@@ -39,7 +39,7 @@ const LoginForm: FC = () => {
                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
                             Inicia sesión en tu cuenta
                         </h1>
-                        <form method='POST' onSubmit={(e) => handleLogin(e)} className="space-y-4 md:space-y-6" autoComplete="off">
+                        <form onSubmit={(e) => handleLogin(e)} className="space-y-4 md:space-y-6" autoComplete="off">
                             <div>
                                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Usuario</label>
                                 <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nombre de usuario" required />
