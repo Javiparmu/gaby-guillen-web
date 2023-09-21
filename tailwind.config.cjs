d99c016fb5f7f8ad2bece0331ec70a8bfdeb8e2a
JaviParMu <mrjavistinson@gmail.com>
2023-09-21 22:20:00 +0200

    test: try without jit mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	mode: 'jit',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
